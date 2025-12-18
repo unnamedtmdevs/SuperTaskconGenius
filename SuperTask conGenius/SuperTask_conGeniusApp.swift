566c01c44c5195d39ad92fc50cb1e80d9e0d4860
ludik <bakhanetssimon@gmail.com>
2025-12-18 20:22:55 +0700

    Initial Commit

---
+++ SuperTask conGenius/SuperTask_conGeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperTask_conGeniusApp.swift
+//  SuperTask conGenius
+//
+//  Created by Simon Bakhanets on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SuperTask_conGeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuperTask conGeniusTests/SuperTask_conGeniusTests.swift
@@ -0,0 +1,16 @@
+//
+//  SuperTask_conGeniusTests.swift
+//  SuperTask conGeniusTests
+//
+//  Created by Simon Bakhanets on 18.12.2025.
+//
+
+import Testing
+
+struct SuperTask_conGeniusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
